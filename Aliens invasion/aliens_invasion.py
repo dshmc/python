e3ab2ab273471d8f61cc7081ee370b427ec6f596
dshmc <dshmc95@gmail.com>
2020-05-19 11:24:42 +0200

    created settings.py

--- Aliens invasion/aliens_invasion.py
+++ Aliens invasion/aliens_invasion.py
@@ -1,21 +1,24 @@
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
     #Инициализация игры и создание объекта экрана
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height) )
     pygame.display.set_caption("Alien Invasion")
+    
 
 
 #Запуск основного цикла игры
 
     while True:
-
+# Отслеживание событий клавиатуры и мыши
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
+        screen.fill(ai_settings.bg_color)
         pygame.display.flip()
 
 run_game()
