960f77b64507af169a191913665fc5a70fe6002e
dshmc <dshmc95@gmail.com>
2020-06-11 21:29:13 +0200

    Adding Points

--- Data visualization with matplotlib/rw_visual.py
+++ Data visualization with matplotlib/rw_visual.py
@@ -4,17 +4,22 @@ from random_walk import RandomWalk
 
 while True:
     
-    rw = RandomWalk()
+    rw = RandomWalk(5000)
     rw.fill_walk()
 
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
         edgecolor = 'none', s=15)
 
-    #Выделение первой и споследней точек
+    # Выделение первой и споследней точек
     plt.scatter(0,0, c = 'green', edgecolor ='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none',
-        s=100)
+        s=1)
+
+    # Удаление осей.
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+    
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
