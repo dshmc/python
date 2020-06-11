3b55e05d0c63c2e28c752c2b40bd59fe517d5ca4
dshmc <dshmc95@gmail.com>
2020-06-11 19:58:41 +0200

    Start and end points

--- Data visualization with matplotlib/rw_visual.py
+++ Data visualization with matplotlib/rw_visual.py
@@ -3,10 +3,18 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-    #Построение случайного блуждания и нанесение точек на диаграму.
+    
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+        edgecolor = 'none', s=15)
+
+    #Выделение первой и споследней точек
+    plt.scatter(0,0, c = 'green', edgecolor ='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none',
+        s=100)
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
