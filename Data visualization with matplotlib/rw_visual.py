bde916f5fa0145d5f3f2b54a7ff288535fbdb808
dshmc <dshmc95@gmail.com>
2020-06-11 19:16:05 +0200

    Generating multiple random walks

--- Data visualization with matplotlib/rw_visual.py
+++ Data visualization with matplotlib/rw_visual.py
@@ -2,8 +2,13 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-#Построение случайного блуждания и нанесение точек на диаграму.
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+while True:
+    #Построение случайного блуждания и нанесение точек на диаграму.
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
