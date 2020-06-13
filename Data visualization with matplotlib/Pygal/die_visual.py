0d7c5b128e6629f2fad36e6b904aea3bf989862d
dshmc <dshmc95@gmail.com>
2020-06-13 21:45:28 +0200

    render diCe_visual.svg

--- Data visualization with matplotlib/Pygal/die_visual.py
+++ Data visualization with matplotlib/Pygal/die_visual.py
@@ -1,18 +1,32 @@
 from die import Die
+import pygal 
 
 # Создание кубика D6
-die = Die()
+die_1 = Die()
+die_2 = Die()
 
 # Моделирование серии бросков с сохранением результатов в списке.
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 
 # Анализ результатов.
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
-print(frequencies)
+
+
+# Визуализация результатов
+hist = pygal.Bar()
+
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6 + D6', frequencies)
+hist.render_to_file('diCe_visual.svg')
