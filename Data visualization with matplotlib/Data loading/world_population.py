11d31e6358e2c7ba0f3858b262d8987d2d372f77
dshmc <dshmc95@gmail.com>
2020-06-20 14:52:59 +0200

    replace

--- Data visualization with matplotlib/Data loading/world_population.py
+++ Data visualization with matplotlib/Data loading/world_population.py
@@ -32,11 +32,11 @@ for cc, pop in cc_populations.items():
 print(len(cc_pops_1), len(cc_pops_2), len(cc_pops3))
 wm_style = RS('#336699', base_style=LCS)
 wm = pygal.maps.world.World(style=wm_style)
-wm.title = 'World Population in 2010, by Country'
+wm.title = 'World Population in 2010, by Coundtry'
 wm.add('0-10m', cc_pops_1)
 wm.add('10m-1bn', cc_pops_2)
 wm.add('>1bn', cc_pops3)
 
-wm.render_to_file('world_population.svg')
+wm.render_to_file('svg/world_population.svg')
 
        
