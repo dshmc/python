56bc63d1d5cdaca7d04529a755f710739b83b53b
dshmc <dshmc95@gmail.com>
2020-06-20 14:49:58 +0200

    World map design

--- Data visualization with matplotlib/Data loading/world_population.py
+++ Data visualization with matplotlib/Data loading/world_population.py
@@ -1,5 +1,6 @@
 import json
 import pygal 
+from pygal.style import LightColorizedStyle as LCS, RotateStyle as RS
 from country_code import get_country_code
 
 #Список заполняется данными.
@@ -18,9 +19,23 @@ for pop_dict in pop_data:
         if code:
             cc_populations[code] = population
 
-            wm = pygal.maps.world.World()
-            wm.title = 'World Population in 2010, by Country'
-            wm.add('2010', cc_populations)
+# Группировка стран по 3 уровням населения.
+cc_pops_1, cc_pops_2, cc_pops3 = {},{},{}
+for cc, pop in cc_populations.items():
+    if pop<1000000:
+        cc_pops_1[cc] = pop
+    elif pop<1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops3[cc] = pop
+# Проверка количества стан на каждом уровне.
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops3))
+wm_style = RS('#336699', base_style=LCS)
+wm = pygal.maps.world.World(style=wm_style)
+wm.title = 'World Population in 2010, by Country'
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops3)
 
 wm.render_to_file('world_population.svg')
 
