1bd1af39d553ddf47bf09e1f93994fbb22de61ff
dshmc <dshmc95@gmail.com>
2020-06-22 18:18:40 +0200

    Visualizing repositories with using Pygal

--- Data visualization with matplotlib/API/python_repos.py
+++ Data visualization with matplotlib/API/python_repos.py
@@ -1,8 +1,10 @@
 import requests
+import pygal 
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
 
 # Создание вызова API и сохранение ответа.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
-
 r = requests.get(url)
 print("Status code:", r.status_code)
 
@@ -12,10 +14,18 @@ print("Total repositories:", response_dict['total_count'])
 
 # Анализ информации о репозиториях.
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
 
-# Анализ первого репозитория.
-repo_dict = repo_dicts[0]
-print("\nKeyd:", len(repo_dict))
-for key in sorted(repo_dict.keys()):
-    print(key)
+names, stars = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Построение визуализации.
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend = False)
+chart.title = 'Most-Starred Python Project on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
+
