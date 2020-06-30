a5a80fbc09374d1bad85c19634698f3a39cd8766
dshmc <dshmc95@gmail.com>
2020-06-30 12:28:02 +0200

    update learning log

---
+++ PYTHON CRASH COURSE/Learning Log/learning_log/learning_logs/urls.py
@@ -0,0 +1,10 @@
+"""Определяет схемы URL для Learning_logs."""
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    #Home page
+    path('', views.index, name='index')
+]
