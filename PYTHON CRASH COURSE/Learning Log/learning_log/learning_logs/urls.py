9e62a264386484e7891eb3acf91a70e742382b44
dshmc <dshmc95@gmail.com>
2020-07-01 11:54:34 +0200

    topics/

--- PYTHON CRASH COURSE/Learning Log/learning_log/learning_logs/urls.py
+++ PYTHON CRASH COURSE/Learning Log/learning_log/learning_logs/urls.py
@@ -6,5 +6,8 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     #Home page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+
+    # Страница со списком всех тем.
+    path('topics/', views.topics, name='topics'),
 ]
