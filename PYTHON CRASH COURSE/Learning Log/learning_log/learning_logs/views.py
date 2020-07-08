eae7ba3f823760baf1909a2bc02bed1e894129f4
dshmc <dshmc95@gmail.com>
2020-07-08 18:30:09 +0200

    Associating data with specific users

--- PYTHON CRASH COURSE/Learning Log/learning_log/learning_logs/views.py
+++ PYTHON CRASH COURSE/Learning Log/learning_log/learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
@@ -7,12 +8,14 @@ def index(request):
     """Домашняя страница приложения Learning Logs"""
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
     """Выводит список тем"""
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Выводит одну тему и все ее записи."""
     topic = Topic.objects.get(id=topic_id)
@@ -20,6 +23,7 @@ def topic(request, topic_id):
     context = {'topic':topic, 'entries':entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """ Определяет новую тему."""
     if request.method != 'POST':
@@ -36,6 +40,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Доюавляет новую запись по конкретной теме."""
     topic = Topic.objects.get(id=topic_id)
@@ -57,6 +62,7 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Редактирует существующую запись."""
     entry = Entry.objects.get(id=entry_id)
