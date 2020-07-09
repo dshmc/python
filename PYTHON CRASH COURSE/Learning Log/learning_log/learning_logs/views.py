c4115fe15b472d40bd0f307f2441d061be6ed567
dshmc <dshmc95@gmail.com>
2020-07-09 18:17:47 +0200

    Protecting user topics

--- PYTHON CRASH COURSE/Learning Log/learning_log/learning_logs/views.py
+++ PYTHON CRASH COURSE/Learning Log/learning_log/learning_logs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -11,7 +12,9 @@ def index(request):
 @login_required
 def topics(request):
     """Выводит список тем"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+    
+    
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
@@ -19,6 +22,8 @@ def topics(request):
 def topic(request, topic_id):
     """Выводит одну тему и все ее записи."""
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic, 'entries':entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -33,7 +38,11 @@ def new_topic(request):
         # Отправлены данные POST; ОБРАБОТАТЬ ДАННЫЕ.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            
+            #form.save()
             return redirect('learning_logs:topics')
     
     # Вывести пустую или недействительную форму.
@@ -67,6 +76,8 @@ def edit_entry(request, entry_id):
     """Редактирует существующую запись."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         #Исходный запрос; форма заполняется данными текущей записи.
