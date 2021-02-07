b378f4c7ba7877ccfbc9ade4da2b7d2535a89e44
Andrii Marynets <42659281+dshmc@users.noreply.github.com>
2021-02-07 19:16:49 +0100

    Update selenium-test.py

--- Selenium/selenium-test.py
+++ Selenium/selenium-test.py
@@ -1,15 +1,15 @@
 from selenium import webdriver
 
-driver_location = "/home/anima/Рабочий стол/Selenium/chromedriver"
+driver_location = "/home/anima/Рабочий стол/Python3/Selenium/chromedriver"
 binary_location = "/usr/bin/google-chrome"
 
 options = webdriver.ChromeOptions()
 options.binary_location = binary_location
 
 driver = webdriver.Chrome(executable_path=driver_location, chrome_options=options)
-driver.get("https://www.imdb.com")
+driver.get("https://www.youtube.com/")
 #driver.get("http://127.0.0.1:8000/admin")
 
 
 print(driver.page_source.encode('utf-8'))
-#driver.quit()
+driver.quit()
